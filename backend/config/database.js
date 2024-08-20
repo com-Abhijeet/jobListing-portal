f334ebf10774fcb80d3130a0329dbad90a90c58b
Sarika Likhar <sarikalikhar11@gmail.com>
2024-08-20 15:52:22 +0530

    api data change

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,15 @@
 import { connect } from 'mongoose';
 
-const Database=()=>{
-connect('mongodb+srv://shindeabhijeet552:Abhijeet143d!@cluster0.pfzykwx.mongodb.net/joblisting-portal?retryWrites=true&w=majority&appName=Cluster0')
+const Database = () => {
+  // connect('mongodb+srv://shindeabhijeet552:Abhijeet143d!@cluster0.pfzykwx.mongodb.net/joblisting-portal?retryWrites=true&w=majority&appName=Cluster0')
+  connect(
+    'mongodb+srv://sarikalikhar11:Sudhirkonge11@cluster0.v6qvq.mongodb.net/'
+  )
     .then(() => {
-        console.log('Connected to MongoDB');
+      console.log('Connected to MongoDB');
     })
     .catch((error) => {
-        console.error('Error connecting to MongoDB:', error);
+      console.error('Error connecting to MongoDB:', error);
     });
-}
+};
 export default Database;
